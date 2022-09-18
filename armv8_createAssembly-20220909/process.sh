2a1d3f110abfc6a31c6afd1177fd498cb999ab16
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2022-09-18 15:12:29 -0300

    main.s movido a su lugar

--- armv8_createAssembly-20220909/process.sh
+++
@@ -1,9 +0,0 @@
-make 
-
-LONG=$(cat main.list | grep -oP '\t[0-9a-f]{8}' | wc -l)
-
-#cat main.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{print awkvar}'
-
-cat main.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{if (NR==1) {print "ROM [0:" awkvar-1 "] =\x27{32\x27h" $1 ","} else { if (NR==awkvar){print "32\x27h" $1 "};"} else {print "32\x27h" $1 ","}}}'
-
-
